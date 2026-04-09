d36e9c5b364b044a7e6edf747787cdcffde318be
RickelviC <Cruzrickelvi@gmail.com>
2026-04-09 12:41:47 -0400

    added user input and discounts

---
+++ src/main/java/com/pluralsight/SandwichPrice.java
@@ -0,0 +1,35 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class SandwichPrice {
+    public static void main(String[] args) {
+
+        double regularPrice = 5.45;
+        double largePrice = 8.95;
+        double discount = 0;
+
+        Scanner user = new Scanner(System.in);
+
+        System.out.print("How old are you?: ");
+        int age = user.nextInt();
+
+        if (age <= 17){
+            discount = .10;
+        } else if (age >= 65) {
+            discount = .20;
+        }
+
+        System.out.println("\nWhat size \n(1) Regular \n(2) Large");
+        int size = user.nextInt();
+
+        if (size == 1){
+            System.out.printf("That is " + "%.2f" , (regularPrice - discount));
+        } else if (size == 2) {
+            System.out.printf("That is " + "%.2f" , (largePrice - discount));
+        } else{
+            System.out.println("Not an option");
+        }
+
+    }
+}
